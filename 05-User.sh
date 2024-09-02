0b8ee542539b9f5818e989800a7276be652a52d4
sai <sai@gmail.com>
2024-09-02 16:23:04 +0530

    rep

--- 05-User.sh
+++ 05-User.sh
@@ -3,3 +3,11 @@
 echo "Please enter the username:"
 
 read USERNAME
+
+echo "the user name entered is $USERNAME"
+
+echo "Please enter your password:"
+
+read -s PASSWORD
+
+echo "the password entered is $PASSWORD"
