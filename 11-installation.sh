1b21b2cdaaf565902e2b169d23c4ebe7d178fa0b
sai <sai@gmail.com>
2024-09-02 20:55:48 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -2,10 +2,16 @@
 
 USERID=$(id -u)
 
-if [ $USERID -eq 0 ]; then
-    dnf install ansible
-else
-    echo "please execute script with root user"
+if [ $USERID -ne 0 ]; then
+    echo "Please run script with root user"
+    exit 1
 fi
 
 dnf list installed ansible
+
+if [ $? -ne 0]; then
+    echo "Ansible is not installed, going to install ansible"
+    dnf install ansible -y
+else
+    echo "Ansible is already installed"
+fi
