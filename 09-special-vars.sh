a633355340369c918d78620663958b0da7bee34e
sai <sai@gmail.com>
2024-09-02 17:34:31 +0530

    special

---
+++ 09-special-vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "All the variables passed to the script is $@"
+echo "Number of varibales passed to the script is $#"
+echo "Script name is $0"
+echo "current working directory is $PWD"
+echo "Home directory of current user is $HOME"
+echo "PID of the executing script is $$"
