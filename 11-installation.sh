0c1d3cde4d39bc3ecd028edd1d816256bd3ee9ed
sai <sai@gmail.com>
2024-09-02 21:03:27 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,9 +9,15 @@ fi
 
 dnf list installed ansible
 
-if [ $? -ne 0]; then
+if [ $? -ne 0 ]; then
     echo "Ansible is not installed, going to install ansible"
     dnf install ansible -y
+    if [ $? -ne 0 ]; then
+        echo "Ansible installation not successful, check it"
+        exit 1
+    else
+        echo "Ansible installation is successfull"
+    fi
 else
     echo "Ansible is already installed"
 fi
