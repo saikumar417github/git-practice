34272c1b709877c6f9506c234e4b025222e5f40a
sai <sai@gmail.com>
2024-09-02 16:03:04 +0530

    four

---
+++ 03-Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi, Suresh , How are you?"
+echo "$PERSON2: Hi, Ramesh, I am fine! .. How are you?"
+echo "$PERSON1: I am fine"
+echo "$PERSON2: Glad to here"
