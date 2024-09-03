002bd1ee825f7ffa39690e8d0c77dc1d09f44256
sai <sai@gmail.com>
2024-09-03 23:21:14 +0530

    for loop installation

---
+++ 13-for-installation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT() {
+    if [ $USERID -ne 0 ]; then
+        echo "Please run this script with root access"
+        exit 1
+    fi
+}
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is .. $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT
